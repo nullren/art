dbbc51afbbc1bbe00b48370d2b64f9956c933d9a
Renning Bruns <renning.bruns@gmail.com>
2018-09-09 09:43:25 -0700

    quick 3d thing with jogl

--- src/main/java/io/github/nullren/art/Hello.java
+++ src/main/java/io/github/nullren/art/Hello.java
@@ -5,19 +5,37 @@ import processing.core.PFont;
 
 public class Hello extends PApplet {
   public void settings() {
-    printArray(PFont.list());
-    size(640, 480);
+//    printArray(PFont.list());
+    size(600, 600, P3D);
   }
 
   public void setup() {
-    PFont comicSans = createFont("ComicSansMS", 32);
-    textFont(comicSans);
-    fill(0);
+//    PFont comicSans = createFont("ComicSansMS", 32);
+//    textFont(comicSans);
+    noStroke();
   }
 
   public void draw() {
-    background(204);
-    text("Hello", mouseX, mouseY);
+    lights();
+    background(0);
+    // go to "middle" of scene
+    translate(width/2, height/2, -height);
+    // rotate due to mouse position
+    float rz = map(mouseY, 0, height, 0, PI);
+    float ry = map(mouseX, 0, width, 0, HALF_PI);
+    rotateZ(rz);
+    rotateY(ry);
+    // add points
+    for (int y = -1; y <= 1; y++) {
+      for (int x = -1; x <= 1; x++) {
+        for (int z = -1; z <= 1; z++) {
+          pushMatrix();
+          translate(300*x, 300*y, -300*z);
+          box(60);
+          popMatrix();
+        }
+      }
+    }
   }
 
   public static void main(String[] args) {
